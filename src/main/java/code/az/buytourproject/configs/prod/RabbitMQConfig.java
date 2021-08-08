dd0b6312d13e6e11925d9d9aec48241b02d83d2f
asadullahjamalov <asadullahnj@code.edu.az>
2021-08-08 15:24:49 +0400

    fix bug

--- src/main/java/code/az/buytourproject/configs/prod/RabbitMQConfig.java
+++ src/main/java/code/az/buytourproject/configs/prod/RabbitMQConfig.java
@@ -1,16 +1,23 @@
-package code.az.buytourproject.configs;
+package code.az.buytourproject.configs.prod;
+
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev")
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Profile("!dev")
 @Configuration
 public class RabbitMQConfig {
 
@@ -24,6 +31,27 @@ public class RabbitMQConfig {
     private String accept_routingKey = "accept_routing_key";
 
     private String exchange = "telegram_bot_exchange";
+
+
+    private String queueName = "buy_tour_web_queue";
+    private String exchangeWeb = "buy_tour_web_exchange";
+    private String routingKey = "buy_tour_web_routing_key";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    DirectExchange exchangeWeb() {
+        return new DirectExchange(exchangeWeb);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchangeWeb) {
+        return BindingBuilder.bind(queue).to(exchangeWeb).with(routingKey);
+    }
+
 
 
     @Bean
@@ -61,11 +89,25 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(acceptQueue).to(exchange).with(accept_routingKey);
     }
 
+
     @Bean
-    public MessageConverter jsonMessageConverter() {
+    public Jackson2JsonMessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    @Bean
+    public ConnectionFactory connectionFactory() throws URISyntaxException {
+        final URI rabbitMqUrl = new URI(System.getenv("CLOUDAMQP_URL"));
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUri(rabbitMqUrl);
+        return factory;
+    }
 
+    @Bean
+    public RabbitTemplate template() throws URISyntaxException {
+        RabbitTemplate temp = new RabbitTemplate(connectionFactory());
+        temp.setMessageConverter(converter());
+        return temp;
+    }
 
 }
---
+++ src/main/java/code/az/buytourproject/configs/prod/RedisConfig.java
@@ -0,0 +1,39 @@
+package code.az.buytourproject.configs.prod;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Profile("!dev")
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        String envRedisUrl = System.getenv("REDIS_URL");
+        URI redisUri = new URI(envRedisUrl);
+        configuration.setPort(redisUri.getPort());
+        configuration.setHostName(redisUri.getHost());
+        configuration.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+
+}
